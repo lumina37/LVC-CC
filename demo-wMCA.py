ebb8f8eecd593a009bc74c3958a37b3f73294dfd
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-02-23 10:34:07 +0800

    fix main

--- demo-wMCA.py
+++ demo-wMCA.py
@@ -1,18 +1,17 @@
 from mcahelper.cfg import node
+from mcahelper.executor import Executor
 from mcahelper.task import CodecTask, Png2yuvTask, PostprocTask, PreprocTask, RenderTask, Yuv2pngTask
 
 node.set_node_cfg('node-cfg.toml')
 
-task1 = PreprocTask(seq_name="Tunnel_Train")
-task2 = Png2yuvTask(frames=1, parent=task1)
-task3 = CodecTask(vtm_type='AI', frames=1, QP=46, parent=task2)
-task4 = Yuv2pngTask(parent=task3)
-task5 = PostprocTask(parent=task4)
-task6 = RenderTask(frames=1, parent=task5)
+seq_name = "Tunnel_Train"
+task1 = PreprocTask(seq_name=seq_name)
+task2 = Png2yuvTask(seq_name=seq_name, frames=1, parent=task1)
+task3 = CodecTask(seq_name=seq_name, vtm_type='AI', frames=1, QP=46, parent=task2)
+task4 = Yuv2pngTask(seq_name=seq_name, parent=task3)
+task5 = PostprocTask(seq_name=seq_name, parent=task4)
+task6 = RenderTask(seq_name=seq_name, frames=1, parent=task5)
 
-task1.run()
-task2.run()
-task3.run()
-task4.run()
-task5.run()
-task6.run()
+if __name__ == "__main__":
+    executor = Executor([task1], process_num=1)
+    executor.run()

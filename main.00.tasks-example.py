19f30b6d939f26843fc6b63aa7ef0a58b0852486
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-06-23 21:21:17 +0800

    chore: update main tasks

--- main.00.tasks-example.py
+++ main.00.tasks-example.py
@@ -1,6 +1,6 @@
 from mcahelper.config import common, node
 from mcahelper.executor import Executor
-from mcahelper.task import CodecTask, Png2yuvTask, PostprocTask, PreprocTask, RenderTask, Yuv2pngTask
+from mcahelper.task import CodecTask, CopyTask, Png2yuvTask, PostprocTask, PreprocTask, RenderTask, Yuv2pngTask
 
 node_cfg = node.set_node_cfg('node-cfg.toml')
 common_cfg = common.set_common_cfg('common-cfg.toml')
@@ -8,12 +8,12 @@ common_cfg = common.set_common_cfg('common-cfg.toml')
 roots = []
 
 for seq_name in node_cfg.cases.seqs:
-    task = RenderTask(seq_name=seq_name, frames=node_cfg.frames)
-    roots.append(task)
+    # Anchor
+    tcopy = CopyTask(seq_name=seq_name, frames=node_cfg.frames)
+    task1 = RenderTask(frames=node_cfg.frames, parent=tcopy)
 
-for seq_name in node_cfg.cases.seqs:
-    task1 = Png2yuvTask(seq_name=seq_name, frames=node_cfg.frames)
-    roots.append(task1)
+    # W/O MCA
+    task1 = Png2yuvTask(frames=node_cfg.frames, parent=tcopy)
 
     for vtm_type in node_cfg.cases.vtm_types:
         for qp in common_cfg.QP.woMCA[seq_name]:
@@ -21,18 +21,18 @@ for seq_name in node_cfg.cases.seqs:
             task3 = Yuv2pngTask(parent=task2)
             task4 = RenderTask(frames=node_cfg.frames, parent=task3)
 
-for seq_name in node_cfg.cases.seqs:
-    task1 = PreprocTask(seq_name=seq_name)
-    roots.append(task1)
+    # # W MCA
+    task1 = PreprocTask(frames=node_cfg.frames, parent=tcopy)
     task2 = Png2yuvTask(frames=node_cfg.frames, parent=task1)
 
     for vtm_type in node_cfg.cases.vtm_types:
         for qp in common_cfg.QP.wMCA[seq_name]:
             task3 = CodecTask(vtm_type=vtm_type, frames=node_cfg.frames, QP=qp, parent=task2)
             task4 = Yuv2pngTask(parent=task3)
-            task5 = PostprocTask(parent=task4)
+            task5 = PostprocTask(frames=node_cfg.frames, parent=task4)
             task6 = RenderTask(frames=node_cfg.frames, parent=task5)
 
+    roots.append(tcopy)
 
 if __name__ == "__main__":
     executor = Executor(roots, process_num=4)

e615cb6e5f5b0527e84ccf6cd411c1f5ccad7bd6
SIGS-TZ <tz23@mails.tsinghua.edu.cn>
2024-07-29 23:34:42 +0800

    feat: revert to legacy compose

--- lvccc/task/compose.py
+++ lvccc/task/compose.py
@@ -15,20 +15,9 @@ from .infomap import query
 class ComposeTask(NonRootTask["ComposeTask"]):
     task: str = "compose"
 
-    frames_per_view: int = 0
-
-    def _post_with_parent(self) -> None:
-        super()._post_with_parent()
-        if self.frames_per_view == 0:
-            self.frames_per_view = self.frames
-
     @functools.cached_property
     def views(self) -> int:
         return self.parent.views
-
-    @functools.cached_property
-    def tag(self) -> str:
-        return f"fpv{self.frames_per_view}"
 
     @functools.cached_property
     def srcdir(self) -> Path:
@@ -38,42 +27,26 @@ class ComposeTask(NonRootTask["ComposeTask"]):
     def _run(self) -> None:
         config = get_config()
 
-        tmp_dstdir = self.dstdir / "tmp"
-        mkdir(tmp_dstdir)
-
-        frame_count = 1
-        frame_idx = 1
-        for view_idx in range(1, self.views**2 + 1):
-            view_fname = f"image_{view_idx:0>3}.png"
-
-            for _ in range(self.frames_per_view):
-                shutil.copyfile(
-                    self.srcdir / f"frame#{frame_idx:0>3}" / view_fname, tmp_dstdir / f"frame#{frame_count:0>3}.png"
-                )
-
-                frame_count += 1
-                frame_idx += 1
-                if frame_idx > self.frames:
-                    frame_idx -= self.frames
+        yuv_dstdir = self.dstdir / "yuv"
+        mkdir(yuv_dstdir)
 
         refimg_path = get_first_file(self.srcdir / "frame#001")
         refimg = cv.imread(str(refimg_path))
         height, width = refimg.shape[:2]
 
-        cmds = [
-            config.app.ffmpeg,
-            "-i",
-            tmp_dstdir / config.default_pattern,
-            "-vf",
-            "format=yuv420p",
-            "-frames:v",
-            self.frames_per_view * (self.views**2),
-            self.dstdir / f"{self.full_tag}-{width}x{height}.yuv",
-            "-v",
-            "warning",
-            "-y",
-        ]
+        for view_idx in range(1, self.views * self.views + 1):
+            cmds = [
+                config.app.ffmpeg,
+                "-i",
+                self.srcdir / "frame#%03d" / f"image_{view_idx:0>3}.png",
+                "-vf",
+                "format=yuv420p",
+                "-frames:v",
+                self.frames,
+                yuv_dstdir / f"{self.full_tag}-v{view_idx:0>3}-{width}x{height}.yuv",
+                "-v",
+                "warning",
+                "-y",
+            ]
 
-        run_cmds(cmds, cwd=tmp_dstdir)
-
-        rm(tmp_dstdir)
+            run_cmds(cmds)
--- lvccc/utils/psnr.py
+++ lvccc/utils/psnr.py
@@ -73,13 +73,21 @@ def calc_mv_psnr(task: ComposeTask) -> np.ndarray:
     views = task.parent.views
     compose_task = ComposeTask().with_parent(RenderTask(views=views).with_parent(copy_task))
 
-    lhs = next(query(compose_task).glob('*.yuv'))
-    rhs = next(query(task).glob('*.yuv'))
+    base_dir = query(compose_task) / "yuv"
+    self_dir = query(task) / "yuv"
 
     width, height = get_render_wh(task)
 
-    psnr = calc_yuv_psnr(lhs, rhs, width, height)
-    return psnr
+    channels = 3
+    accpsnr = np.zeros(channels)
+
+    count = 0
+    for lhs, rhs in zip(base_dir.iterdir(), self_dir.iterdir(), strict=True):
+        accpsnr += calc_yuv_psnr(lhs, rhs, width, height)
+        count += 1
+    accpsnr /= count
+
+    return accpsnr
 
 
 def calc_lenslet_psnr(task: ComposeTask) -> np.ndarray:

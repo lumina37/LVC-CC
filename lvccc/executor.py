c54487f2cd59a16a5aa93efa2376fe099cde612f
lumina37 <starry.qvq@gmail.com>
2025-01-03 12:15:12 +0800

    feat: simplify executor

--- lvccc/executor.py
+++ lvccc/executor.py
@@ -13,11 +13,9 @@ class Executor:
 
     infomap: TypeInfomap = dcs.field(init=False, default_factory=get_infomap)
     task_queue: queue.Queue = dcs.field(init=False, default_factory=queue.Queue)
-    active_count: int = dcs.field(init=False, default=0)
-    active_count_lock: threading.Lock = dcs.field(init=False, default_factory=threading.Lock)
-    pending_count: int = dcs.field(init=False, default=0)
-    pending_count_lock: threading.Lock = dcs.field(init=False, default_factory=threading.Lock)
-    waitfor_task_cond: threading.Condition = dcs.field(init=False, default_factory=threading.Condition)
+    task_cnt: int = dcs.field(init=False, default=0)
+    task_cnt_lock: threading.Lock = dcs.field(init=False, default_factory=threading.Lock)
+    cond: threading.Condition = dcs.field(init=False, default_factory=threading.Condition)
 
     def _exit_ready(self) -> bool:
         """
@@ -28,18 +26,11 @@ class Executor:
             False if there might have some tasks to do and the executor is not ready for exit.
         """
 
-        zero_count = 0
+        with self.task_cnt_lock:
+            if self.task_cnt == 0:
+                return True
 
-        with self.active_count_lock:
-            if self.active_count == 0:
-                zero_count += 1
-
-        with self.pending_count_lock:
-            if self.pending_count == 0:
-                zero_count += 1
-
-        exit_allowed = zero_count == 2
-        return exit_allowed
+        return False
 
     def _worker(self):
         while 1:
@@ -49,30 +40,27 @@ class Executor:
             except queue.Empty:  # noqa: PERF203
                 if self._exit_ready():
                     return
-                with self.waitfor_task_cond:
-                    self.waitfor_task_cond.wait()
+                with self.cond:
+                    self.cond.wait()
 
             else:
-                with self.active_count_lock:
-                    self.active_count += 1
-                with self.pending_count_lock:
-                    self.pending_count += len(task.children)
-                    self.pending_count -= 1
+                with self.task_cnt_lock:
+                    self.task_cnt += len(task.children)
 
                 task.run()
 
                 if task.children:
                     for child in task.children:
                         self.task_queue.put(child, block=False)
-                    with self.waitfor_task_cond:
-                        self.waitfor_task_cond.notify(len(task.children))
+                    with self.cond:
+                        self.cond.notify(len(task.children))
 
-                with self.active_count_lock:
-                    self.active_count -= 1
+                with self.task_cnt_lock:
+                    self.task_cnt -= 1
 
                 if self._exit_ready():
-                    with self.waitfor_task_cond:
-                        self.waitfor_task_cond.notify_all()
+                    with self.cond:
+                        self.cond.notify_all()
 
     def run(self) -> None:
         if self.process_num < 1:
@@ -85,7 +73,7 @@ class Executor:
 
         for root in roots:
             self.task_queue.put(root)
-        self.pending_count = len(roots)
+        self.task_cnt = len(roots)
 
         workers = [threading.Thread(target=self._worker) for _ in range(self.process_num)]
 

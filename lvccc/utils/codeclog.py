2e9f355405d00f7fbcfe13af89fb7958471a36ee
lumina37 <starry.qvq@gmail.com>
2025-01-19 15:58:16 +0800

    perf: codec log parser

--- lvccc/utils/codeclog.py
+++ lvccc/utils/codeclog.py
@@ -4,35 +4,42 @@ import dataclasses as dcs
 import re
 from typing import TYPE_CHECKING, TextIO
 
+from ..logging import get_logger
+
 if TYPE_CHECKING:
     from pathlib import Path
 
 
 @dcs.dataclass
 class CodecLog:
-    bitrate: float
-    timecost: float
+    bitrate: float = 0.0
+    timecost: float = 0.0
 
     @staticmethod
     def load(f: TextIO) -> CodecLog:
-        layerid_row_idx = -128
-
-        for i, row in enumerate(f):
+        is_completed = False
+        for row in f:
             if row.startswith("LayerId"):
-                layerid_row_idx = i
+                is_completed = True
+                break
+        if not is_completed:
+            logger = get_logger()
+            logger.warning(f"The codec log is incomplete: name={f.name}")
+            return CodecLog()
 
-            if layerid_row_idx > 0:
-                if i == layerid_row_idx + 2:
-                    matchobj = re.findall(r"\d+\.?\d*", row)
-                    bitrate_str = matchobj[1]
-                    bitrate = float(bitrate_str)
-                    continue
+        f.readline()  # skip the table header
 
-                if i == layerid_row_idx + 5:
-                    matchobj = re.search(r"Time:\s+(\d+\.?\d*)", row)
-                    timecost_str = matchobj.group(1)
-                    timecost = float(timecost_str)
-                    continue
+        matchobj = re.findall(r"\d+\.?\d*", f.readline())
+        bitrate_str = matchobj[1]
+        bitrate = float(bitrate_str)
+
+        timecost = 0.0
+        for row in f:
+            if not row.startswith(" Total Time"):
+                continue
+            matchobj = re.search(r"(\d+\.?\d*) sec", row)
+            timecost_str = matchobj.group(1)
+            timecost = float(timecost_str)
 
         return CodecLog(bitrate, timecost)
 

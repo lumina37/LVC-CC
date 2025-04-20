39fda3bcf3b7aab885ee289c6364c7492536516e
lumina37 <starry.qvq@gmail.com>
2025-04-20 12:16:57 +0800

    feat: remove all find qp facilities

--- cc-30-export-figure.py
+++ cc-30-export-figure.py
@@ -36,18 +36,15 @@ proc_color = "crimson"
 
 for seq_name in config.seqs:
     tcopy = CopyTask(seq_name=seq_name, frames=config.frames)
-    crop_size = config.proc["crop_size"][seq_name]
-    tpreproc = PreprocTask(crop_size=crop_size).follow(tcopy)
+    tpreproc = PreprocTask().follow(tcopy)
 
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax: Axes = ax
-    ax.set_xlabel("Total bitrate (Kbps)")
-    ax.set_ylabel("PSNR (dB)")
-    ax.set_title(seq_name)
+    anchorQPs = config.anchorQP.get(seq_name, None)
+    if not anchorQPs:
+        continue
 
     anchor_bitrates = []
-    anchor_psnrs = []
-    anchorQPs = config.anchorQP.get(seq_name, [])
+    anchor_mvpsnrs = []
+    anchor_llpsnrs = []
     for qp in anchorQPs:
         tcodec = CodecTask(qp=qp).follow(tcopy)
         tconvert = Convert40Task(views=config.views).follow(tcodec)
@@ -60,15 +57,15 @@ for seq_name in config.seqs:
             metrics: dict = json.load(f)
 
         anchor_bitrates.append(metrics["bitrate"])
-        anchor_psnrs.append(metrics["mvpsnr_y"])
+        anchor_mvpsnrs.append(metrics["mvpsnr_y"])
+        anchor_llpsnrs.append(metrics["llpsnr_y"])
 
-    ax.plot(anchor_bitrates, anchor_psnrs, label="anchor", color=anchor_color)
-    for i in range(len(anchor_bitrates)):
-        ax.annotate(str(anchorQPs[i]), xy=(anchor_bitrates[i], anchor_psnrs[i]), color=anchor_color)
+    crop_size = config.proc["crop_size"][seq_name]
+    tpreproc = PreprocTask(crop_size=crop_size).follow(tcopy)
 
-    # With Pre/Postprocess
     proc_bitrates = []
-    proc_psnrs = []
+    proc_mvpsnrs = []
+    proc_llpsnrs = []
     procQPs = config.proc["QP"].get(seq_name, [])
     for qp in procQPs:
         tcodec = CodecTask(qp=qp).follow(tpreproc)
@@ -83,12 +80,50 @@ for seq_name in config.seqs:
             metrics: dict = json.load(f)
 
         proc_bitrates.append(metrics["bitrate"])
-        proc_psnrs.append(metrics["mvpsnr_y"])
+        proc_mvpsnrs.append(metrics["mvpsnr_y"])
+        proc_llpsnrs.append(metrics["llpsnr_y"])
+        procQPs.append(qp)
 
-    ax.plot(proc_bitrates, proc_psnrs, label="proc", color=proc_color)
+    # Multi-view
+    mvfig, mvax = plt.subplots(figsize=(8, 6))
+    mvax: Axes = mvax
+    mvax.set_xlabel("Total bitrate (Kbps)")
+    mvax.set_ylabel("Multi-view PSNR (dB)")
+    mvtitle = f"{tpreproc.tag}-multiview"
+    mvax.set_title(mvtitle)
+
+    mvax.plot(anchor_bitrates, anchor_mvpsnrs, label="anchor", color=anchor_color, marker="o", markersize=1)
+    for i in range(len(anchor_bitrates)):
+        mvax.annotate(str(anchorQPs[i]), xy=(anchor_bitrates[i], anchor_mvpsnrs[i]), color=anchor_color)
+
+    mvax.plot(proc_bitrates, proc_mvpsnrs, label="proc", color=proc_color, marker="o", markersize=1)
     for i in range(len(proc_bitrates)):
-        ax.annotate(str(procQPs[i]), xy=(proc_bitrates[i], proc_psnrs[i]), color=proc_color)
+        mvax.annotate(str(procQPs[i]), xy=(proc_bitrates[i], proc_mvpsnrs[i]), color=proc_color)
 
-    ax.legend()
+    mvax.legend()
 
-    fig.savefig((dst_dir / seq_name).with_suffix(".svg"))
+    mvfig.savefig((dst_dir / mvtitle).with_suffix(".svg"))
+
+    plt.close(mvfig)
+
+    # Lenslet
+    llfig, llax = plt.subplots(figsize=(8, 6))
+    llax: Axes = llax
+    llax.set_xlabel("Total bitrate (Kbps)")
+    llax.set_ylabel("Lenslet PSNR (dB)")
+    lltitle = f"{tpreproc.tag}-lenslet"
+    llax.set_title(lltitle)
+
+    llax.plot(anchor_bitrates, anchor_llpsnrs, label="anchor", color=anchor_color, marker="o", markersize=1)
+    for i in range(len(anchor_bitrates)):
+        llax.annotate(str(anchorQPs[i]), xy=(anchor_bitrates[i], anchor_llpsnrs[i]), color=anchor_color)
+
+    llax.plot(proc_bitrates, proc_llpsnrs, label="proc", color=proc_color, marker="o", markersize=1)
+    for i in range(len(proc_bitrates)):
+        llax.annotate(str(procQPs[i]), xy=(proc_bitrates[i], proc_llpsnrs[i]), color=proc_color)
+
+    llax.legend()
+
+    llfig.savefig((dst_dir / lltitle).with_suffix(".svg"))
+
+    plt.close(llfig)

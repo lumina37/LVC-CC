ce02f3f1b88e70e5d0c1b6255baf7d8fea73b15a
lumina37 <starry.qvq@gmail.com>
2025-04-30 09:01:10 +0800

    feat: add posetrace script

---
+++ cc-03-run-anchor-posetrace.py
@@ -0,0 +1,39 @@
+import argparse
+from pathlib import Path
+
+from lvccc.config import update_config
+from lvccc.executor import Executor
+from lvccc.task import CodecTask, Convert40Task, CopyTask, PosetraceTask
+from lvccc.utils import avaliable_cpu_count
+
+# Config from CMD
+parser = argparse.ArgumentParser(description="Anchor: convert/codec+convert")
+
+parser.add_argument("--configs", "-c", nargs="+", type=str, default="", help="list of config file path")
+parser.add_argument(
+    "--base", "-b", type=str, default="base.toml", help="base config, recommended for per-device settings"
+)
+parser.add_argument("--threads", "-j", type=int, default=avaliable_cpu_count() // 2, help="use how many threads")
+opt = parser.parse_args()
+
+config = update_config(Path(opt.base))
+for cpath in opt.configs:
+    config = update_config(Path(cpath))
+
+
+# Task forest
+roots = []
+
+for seq_name in config.seqs:
+    tcopy = CopyTask(seq_name=seq_name, frames=config.frames)
+    roots.append(tcopy)
+
+    for qp in config.anchorQP.get(seq_name, []):
+        tcodec = CodecTask(qp=qp).follow(tcopy)
+        tconvert = Convert40Task(views=config.views).follow(tcodec)
+        tposetrace = PosetraceTask(frame_per_view=12).follow(tconvert)
+
+
+if __name__ == "__main__":
+    executor = Executor(roots, process_num=opt.threads)
+    executor.run()
